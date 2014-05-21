c01edd18a2e195d77f53fd6b0f5d1c2bfbdf853f
Paul Bouman <boumanpaul@gmail.com>
2014-05-21 11:44:30 +0200

    Update adreno libs KK_2.7_RB1.04.04.00.007.018 from LegacyXperia
    From d2lte.
    Tag AU_LINUX_ANDROID_KK_2.7_RB1.04.04.00.007.018

--- qcom-vendor-blobs.mk
+++ qcom-vendor-blobs.mk
@@ -37,16 +37,17 @@ PRODUCT_COPY_FILES += \
     vendor/qcom/proprietary/system/etc/firmware/yamato_pfp.fw:/system/etc/firmware/yamato_pfp.fw \
     vendor/qcom/proprietary/system/etc/firmware/yamato_pm4.fw:/system/etc/firmware/yamato_pm4.fw \
     vendor/qcom/proprietary/system/lib/egl/egl.cfg:/system/lib/egl/egl.cfg \
-    vendor/qcom/proprietary/system/lib/egl/eglsubAndroid.so:/system/lib/egl/eglsubAndroid.so \
-    vendor/qcom/proprietary/system/lib/egl/libEGL_adreno200.so:/system/lib/egl/libEGL_adreno200.so \
-    vendor/qcom/proprietary/system/lib/egl/libGLESv1_CM_adreno200.so:/system/lib/egl/libGLESv1_CM_adreno200.so \
-    vendor/qcom/proprietary/system/lib/egl/libGLESv2_adreno200.so:/system/lib/egl/libGLESv2_adreno200.so \
-    vendor/qcom/proprietary/system/lib/egl/libplayback_adreno200.so:/system/lib/egl/libplayback_adreno200.so \
-    vendor/qcom/proprietary/system/lib/egl/libq3dtools_adreno200.so:/system/lib/egl/libq3dtools_adreno200.so \
-    vendor/qcom/proprietary/system/lib/libC2D2.so:/system/lib/libC2D2.so \
-    vendor/qcom/proprietary/system/lib/libc2d2_z180.so:/system/lib/libc2d2_z180.so \
-    vendor/qcom/proprietary/system/lib/libgsl.so:/system/lib/libgsl.so \
-    vendor/qcom/proprietary/system/lib/libllvm-arm.so:/system/lib/libllvm-arm.so \
-    vendor/qcom/proprietary/system/lib/libOpenCL.so:/system/lib/libOpenCL.so \
-    vendor/qcom/proprietary/system/lib/libOpenVG.so:/system/lib/libOpenVG.so \
-    vendor/qcom/proprietary/system/lib/libsc-a2xx.so:/system/lib/libsc-a2xx.so
+    vendor/qcom/proprietary/system/vendor/lib/egl/eglsubAndroid.so:/system/vendor/lib/egl/eglsubAndroid.so \
+    vendor/qcom/proprietary/system/vendor/lib/egl/libEGL_adreno.so:/system/vendor/lib/egl/libEGL_adreno.so \
+    vendor/qcom/proprietary/system/vendor/lib/egl/libGLESv1_CM_adreno.so:/system/vendor/lib/egl/libGLESv1_CM_adreno.so \
+    vendor/qcom/proprietary/system/vendor/lib/egl/libGLESv2_adreno.so:/system/vendor/lib/egl/libGLESv2_adreno.so \
+    vendor/qcom/proprietary/system/vendor/lib/egl/libGLESv2S3D_adreno.so:/system/vendor/lib/egl/libGLESv2S3D_adreno.so \
+    vendor/qcom/proprietary/system/vendor/lib/egl/libq3dtools_adreno.so:/system/vendor/lib/egl/libq3dtools_adreno.so \
+    vendor/qcom/proprietary/system/vendor/lib/libadreno_utils.so:/system/vendor/lib/libadreno_utils.so \
+    vendor/qcom/proprietary/system/vendor/lib/libC2D2.so:/system/vendor/lib/libC2D2.so \
+    vendor/qcom/proprietary/system/vendor/lib/libc2d2_z180.so:/system/vendor/lib/libc2d2_z180.so \
+    vendor/qcom/proprietary/system/vendor/lib/libCB.so:/system/vendor/lib/libCB.so \
+    vendor/qcom/proprietary/system/vendor/lib/libgsl.so:/system/vendor/lib/libgsl.so \
+    vendor/qcom/proprietary/system/vendor/lib/libOpenCL.so:/system/vendor/lib/libOpenCL.so \
+    vendor/qcom/proprietary/system/vendor/lib/libOpenVG.so:/system/vendor/lib/libOpenVG.so \
+    vendor/qcom/proprietary/system/vendor/lib/libsc-a2xx.so:/system/vendor/lib/libsc-a2xx.so
